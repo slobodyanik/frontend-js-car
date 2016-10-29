5db38e93cac8183fcf0c28facb39525650e47d84
constgen <const.gen@gmail.com>
2016-10-30 01:27:42 +0300

    Added new tests * проверить на лишний вызов car.land() во время car.put() * проверить что при car.drive() и passengers: [] - скорость остаётся не изменной, а может лучше и 0 * сбрасывать скорость на 0 если высадить всех пассажиров на ходу * проверить что машина не меняет скорость, когда кого-то садят * проверить что машина не меняет скорость, когда кого-то высаживают

--- test/spec.js
+++ test/spec.js
@@ -3,146 +3,189 @@
 var car = require('../src/js/car.js');
 
 describe('Module "car"', function () {
-	
-	beforeEach(function(){
+
+	beforeEach(function () {
 		spyOn(car, 'drive').and.callThrough()
 		spyOn(car, 'put').and.callThrough()
 		spyOn(car, 'land').and.callThrough()
 	})
-	
-	describe('has a correct interface with a property', function(){
-		it('"color" that is a String', function(){
-			expect(car.color).toEqual(jasmine.any(String))	
+
+	describe('has a correct interface with a property', function () {
+		it('"color" that is a String', function () {
+			expect(car.color).toEqual(jasmine.any(String))
 		})
-		it('"dours" that is a Number', function(){
-			expect(car.dours).toEqual(jasmine.any(Number))	
+		it('"dours" that is a Number', function () {
+			expect(car.dours).toEqual(jasmine.any(Number))
 		})
-		it('"passengers" that is a Array', function(){
-			expect(car.passengers).toEqual(jasmine.any(Array))	
+		it('"passengers" that is a Array', function () {
+			expect(car.passengers).toEqual(jasmine.any(Array))
 		})
-		it('"seats" that is a Number', function(){
-			expect(car.seats).toEqual(jasmine.any(Number))	
+		it('"seats" that is a Number', function () {
+			expect(car.seats).toEqual(jasmine.any(Number))
 		})
-		it('"maxSpeed" that is a Number', function(){
-			expect(car.maxSpeed).toEqual(jasmine.any(Number))	
+		it('"maxSpeed" that is a Number', function () {
+			expect(car.maxSpeed).toEqual(jasmine.any(Number))
 		})
-		it('"defaultSpeed" that is a Number', function(){
-			expect(car.defaultSpeed).toEqual(jasmine.any(Number))	
+		it('"defaultSpeed" that is a Number', function () {
+			expect(car.defaultSpeed).toEqual(jasmine.any(Number))
 		})
-		it('"speed" that is a Number', function(){
-			expect(car.speed).toEqual(jasmine.any(Number))	
+		it('"speed" that is a Number', function () {
+			expect(car.speed).toEqual(jasmine.any(Number))
 		})
-		it('"drive" that is a Function', function(){
-			expect(car.drive).toEqual(jasmine.any(Function))	
+		it('"drive" that is a Function', function () {
+			expect(car.drive).toEqual(jasmine.any(Function))
 		})
-		it('"put" that is a Function', function(){
-			expect(car.put).toEqual(jasmine.any(Function))	
+		it('"put" that is a Function', function () {
+			expect(car.put).toEqual(jasmine.any(Function))
 		})
-		it('"land" that is a Function', function(){
-			expect(car.land).toEqual(jasmine.any(Function))	
-		})
-	})
-	
-	describe('by default has a property', function(){
-		it('"color" equal "white"', function(){
-			expect(car.color).toEqual("white")	
-		})
-		it('"dours" equal 4', function(){
-			expect(car.dours).toEqual(4)	
-		})
-		it('"passengers" that is empty', function(){
-			expect(car.passengers).toEqual([])	
-		})
-		it('"seats" equal 4', function(){
-			expect(car.seats).toEqual(4)	
-		})
-		it('"maxSpeed" equal 100', function(){
-			expect(car.maxSpeed).toEqual(100)	
-		})
-		it('"defaultSpeed" equal 60', function(){
-			expect(car.defaultSpeed).toEqual(60)	
-		})
-		it('"speed" equal 0', function(){
-			expect(car.speed).toEqual(0)	
+		it('"land" that is a Function', function () {
+			expect(car.land).toEqual(jasmine.any(Function))
 		})
 	})
-	
-	describe('pessangers', function(){
-		afterEach(function(){
+
+	describe('by default has a property', function () {
+		it('"color" equal "white"', function () {
+			expect(car.color).toEqual("white")
+		})
+		it('"dours" equal 4', function () {
+			expect(car.dours).toEqual(4)
+		})
+		it('"passengers" that is empty', function () {
+			expect(car.passengers).toEqual([])
+		})
+		it('"seats" equal 4', function () {
+			expect(car.seats).toEqual(4)
+		})
+		it('"maxSpeed" equal 100', function () {
+			expect(car.maxSpeed).toEqual(100)
+		})
+		it('"defaultSpeed" equal 60', function () {
+			expect(car.defaultSpeed).toEqual(60)
+		})
+		it('"speed" equal 0', function () {
+			expect(car.speed).toEqual(0)
+		})
+	})
+
+	describe('pessangers', function () {
+		afterEach(function () {
 			car.speed = 0
 			car.seats = 8
 			car.maxSpeed = 200
 			car.defaultSpeed = 150
 			car.passengers.length = 0;
-		})	
-		
-		it('can be put', function(){
-			car.put()
-			car.put()
-			
-			expect(car.passengers.length).toEqual(2)	
 		})
-		it('can be landed', function(){
+
+		it('can be put', function () {
+			car.put()
+			car.put()
+
+			expect(car.passengers.length).toEqual(2)
+		})
+		it('can be landed', function () {
+			car.put()
 			car.put()
 			car.land()
-			
-			expect(car.passengers.length).toEqual(0)	
+
+			expect(car.passengers.length).toEqual(1)
 		})
-		it('can\'t be put more than seats', function(){
+		it('can\'t be put more than seats', function () {
 			var times = car.seats * 2;
 			while (times--) {
 				car.put()
 			}
-			
-			expect(car.passengers.length).toEqual(car.seats)	
+
+			expect(car.passengers.length).toEqual(car.seats)
+		})
+		it('are not landed if one more passengers are put than possible', function () {
+			var times = car.seats * 2;
+			while (times--) {
+				car.put()
+			}
+
+			expect(car.land).not.toHaveBeenCalled()
 		})
 	})
-	
-	describe('when drived', function(){
-		afterEach(function(){
+
+	describe('when drived', function () {
+		afterEach(function () {
 			car.speed = 0
 			car.seats = 8
 			car.maxSpeed = 200
 			car.defaultSpeed = 150
 			car.passengers.length = 0;
-		})	
-		
-		it('can\'t drive without a driver', function(){
-			car.drive()
-			
-			expect(car.speed).toEqual(0)	
 		})
-		it('can drive with a driver', function(){
+
+		it('can\'t drive without a driver', function () {
+			var speed = 20
+			car.drive(speed)
+
+			expect(car.speed).toEqual(0)
+		})
+		it('can drive with a driver', function () {
 			car.put()
 			car.drive()
-			
-			expect(car.speed).toBeGreaterThan(0)	
+
+			expect(car.speed).toBeGreaterThan(0)
 		})
-		it('changes speed', function(){
+		it('changes speed', function () {
 			var speed = 85;
 			car.put()
 			car.drive(speed)
-			
-			expect(car.speed).toEqual(speed)	
+
+			expect(car.speed).toEqual(speed)
 		})
-		it('doesn\'t change speed without a driver', function(){
+		it('can be stopped', function () {
 			var speed = 85;
+			car.put()
 			car.drive(speed)
-			
-			expect(car.speed).toEqual(0)	
+			car.drive(0)
+
+			expect(car.speed).toEqual(0)
 		})
-		it('has a default speed', function(){
+		it('doesn\'t change speed without a driver', function () {
+			var speed = 85
+			car.drive(speed)
+
+			expect(car.speed).toEqual(0)
+		})
+		it('doesn\'t change speed if additional passenger is put', function () {
+			var speed = 85
+			car.put()
+			car.drive(speed)
+			car.put()
+
+			expect(car.speed).toEqual(speed)
+		})
+		it('doesn\'t change speed if some passenger is landed', function () {
+			var speed = 85
+			car.put()
+			car.put()
+			car.drive(speed)
+			car.land()
+
+			expect(car.speed).toEqual(speed)
+		})
+		it('has a default speed', function () {
 			car.put()
 			car.drive()
-			
-			expect(car.speed).toEqual(car.defaultSpeed)	
+
+			expect(car.speed).toEqual(car.defaultSpeed)
 		})
-		it('is limited to max speed', function(){
+		it('is limited to max speed', function () {
 			var speed = car.maxSpeed + 85;
 			car.put()
 			car.drive(speed)
-			
+
 			expect(car.speed).toEqual(car.maxSpeed)
+		})
+		it('stopped if all passengers are landed', function () {
+			var speed = 20
+			car.put()
+			car.drive(speed)
+			car.land()
+
+			expect(car.speed).toEqual(0)
 		})
 	})
 })
